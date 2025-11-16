b5c0ea5f6d5b635437d865a178608c7b78e1e8b6
Islombek Matkarimov <matkaimovislambek@gmail.com>
2025-11-16 13:24:05 +0500

    loader qo'shildi

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+	const loaderWrapper = document.querySelector('.loader-wrapper')
+
+	setTimeout(() => {
+		loaderWrapper.style.display = 'none'
+	}, 1700)
+})
